4d9421ec257099f0207a1caae0c37b98833cb8b5
Reo Liu <liudev555@gmail.com>
2018-12-04 18:32:13 +0800

    Fixes in suggestions and relations trigger

--- src/triggers.js
+++ src/triggers.js
@@ -3,7 +3,7 @@ export const PERSON = 2;
 export const RELATIONS = 3;
 export const TAG_TRIGGER = '#';
 export const PERSON_TRIGGER = '@';
-export const RELATIONS_TRIGGER = '<';
+export const RELATIONS_TRIGGER = '<>';
 
 export const TAG_REG_EX = /^#/;
 export const PERSON_REG_EX = /^@/;
